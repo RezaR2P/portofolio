5f2fa881457a9c34308a441044c5f3eda8902ba9
RezaR2P <kecilreza575@gmail.com>
2025-02-27 11:41:26 +0700

    feat: Tambahkan komponen Home dengan informasi perkenalan

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LoadingScreen from './components/LoadingScreen.jsx'
 import './index.css'
 import Navbar from './components/Navbar.jsx'
 import MobileMenu from './components/MobileMenu.jsx'
+import Home from './components/sections/Home.jsx'
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false)
@@ -13,6 +14,7 @@ function App() {
       <div className={`min-h-screen transition-opacity duration-1000 ${isLoaded ? 'opacity-100' : 'opacity-0'} bg-black text-gray-100`}>
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
       </div>
     </>
   )
